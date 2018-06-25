bb016f2e6a16a3f13627d2ab8d64bf5d1cd8d8ff
souravdas25 <souravbumbadas25@gmail.com>
2018-06-25 14:13:36 +0530

    Cleared

--- resources/views/vendor/voyager/formfields/rich_text_box.blade.php
+++
@@ -1,7 +0,0 @@
-<textarea @if($row->required == 1) required @endif class="form-control richTextBox" name="{{ $row->field }}" id="richtext{{ $row->field }}">
-    @if(isset($dataTypeContent->{$row->field}))
-        {{ old($row->field, $dataTypeContent->{$row->field}) }}
-    @else
-        {{old($row->field)}}
-    @endif
-</textarea>
